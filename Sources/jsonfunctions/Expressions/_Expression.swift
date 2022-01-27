4d84c0e3f1c2375329607766582e3456e3f29d49
Katharina Seiz <katharina.seiz@sap.com>
2022-01-27 13:16:50 +0100

    correct copyright info

--- Sources/jsonfunctions/Expressions/_Expression.swift
+++ Sources/jsonfunctions/Expressions/_Expression.swift
@@ -1,7 +1,18 @@
 //
 // json-functions-swift
 //
-// Copyright (c) 2022 SAP SE or an SAP affiliate company
+// Parts of this file are copied from file
+// https://github.com/eu-digital-green-certificates/json-logic-swift/blob/master/Sources/jsonlogic/Parser.swift
+// in forked Repository
+// https://github.com/eu-digital-green-certificates/json-logic-swift
+//
+//  Parser.swift
+//  jsonlogic
+//
+//  Created by Christos Koninis on 16/06/2018.
+//  Licensed under MIT
+//
+// Modifications Copyright (c) 2022 SAP SE or an SAP affiliate company
 //
 
 import Foundation
